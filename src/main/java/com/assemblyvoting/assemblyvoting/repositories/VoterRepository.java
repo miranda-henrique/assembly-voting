3e72ddd9c1a9f679078e1936d1db1be707818e22
Henrique Miranda <miranda.henrique@outlook.com.br>
2023-05-14 19:27:29 -0300

    voterRepository and dateConfig

---
+++ src/main/java/com/assemblyvoting/assemblyvoting/repositories/VoterRepository.java
@@ -0,0 +1,10 @@
+package com.assemblyvoting.assemblyvoting.repositories;
+
+import com.assemblyvoting.assemblyvoting.models.VoterModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface VoterRepository extends JpaRepository<VoterModel, UUID> {
+
+}
